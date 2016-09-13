87ddeda3893188ac575462ecb0977cd89699d24d
Davin Glick <davinglick@davins-macbook.local>
2016-09-13 16:30:45 +1000

    changed background colour and added constant

---
+++ Australia Construction Products/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Australia Construction Products
+//
+//  Created by Davin Glick on 13/09/2016.
+//  Copyright © 2016 Davin Glick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let userName = "Davin"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
